21f207f920bbca04a15dab11805a9951ddc876d6
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-01-08 17:47:45 +0530

    Add files via upload

---
+++ common/js/movies.js
@@ -0,0 +1,148 @@
+var ms="https://dl.gdc04.workers.dev/0:/"; 
+
+/*
+bollywood=1
+southMovies=2
+engMovieHindiDubbed=3
+*/
+var movies=
+[
+/*['name',`${ms}lnk`,mid,'img', cat],*/
+/*['name',`${ms}lnk`,mid,'img', cat],*/
+// ['Escape Room','32Wbwqm',`${ms}HollyWood/Escape.Room.2017/Escape.Room.2017.Hindi.English.480p.BluRay.ESub-DudeFilms.in.mkv`,3],
+['Endangered Species','3pUPIUU',`${ms}HollyWood/Endangered.Species.2021/Endangered.Species.2021.Hindi.English.480p.BlyRay.ESub-DudeFilms.in.mkv`,3],
+['Murder At...302','32P6jR4',`${ms}BollyWood/Murder.At.Teesri.Manzil.302/Murder.At.Teesri.Manzil.302.2009.Hindi.480p.WEB-Dl.ESub-DudeFilms.in.mkv`,1],
+['Wrong Turn','3mKwU90',`${ms}HollyWood/Wrong.Turn.2021/Wrong.Turn.2021.Hindi.English.480p.BluRay.ESub-DudeFilms.in.mkv`,3],
+['Old','3FVgXUU',`${ms}HollyWood/Old.2021/Old.2021.Hindi.English.480p.BluRay.ESub-DudeFilms.in.mkv`,3],
+['Waah Zindagi','32IPreN',`${ms}BollyWood/Zee5/Waah.Zindagi.2021/Waah.Zindagi.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+['83','3przmmP',`${ms}BollyWood/83.2021.HDCam/83.2021.Hindi.480p.Pre.DVDRip-DudeFilms.in.mkv`,1],
+['Encanto','3mxW3nk',`${ms}HollyWood/Encanto.2021/Encanto.2021.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
+['Minnal Murali','3sNMUev',`${ms}SouthMovies/Minnal.Murali.2021/Minnal.Murali.2021.Hindi-Malayalam.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
+['Atrangi Re','33WZsFR',`${ms}BollyWood/hotstar/Atrangi.Re.2021/Atrangi.Re.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+['Switchh','3EtC9jo',`${ms}BollyWood/Switchh.2021/Switchh.2021.Hindi.480p.WEB-DL-DudeFilms.in.mkv`,1],
+
+['The Matrix','33T06UR',`${ms}HollyWood/The.Matrix.Resurrections.2021/The.Matrix.Resurrections.2021.480p.WEBRip.Hindi.CAM-English.ESub.x264-DudeFilms.in.mkv`, 3],
+['Resident Evil...Raccoon','33TXZA8',`${ms}HollyWood/Resident.Evil.Welcome.to.Raccoon.City.2021/Resident.Evil.Welcome.to.Raccoon.City.2021.Hindi(CAM)-English.480p.BluRay.ESub-DudeFilms.in.mkv`, 3],
+['Shock Wave','3Fo7vcJ',`${ms}HollyWood/Shock.Wave.2.2020/Shock.Wave.2.2020.Hindi.Chinese.480p.BluRay.ESub-DudeFilms.in.mkv`, 3],
+['Pushpa','3IY2pWB',`${ms}South.Movies/Pushpa.The.Rise-Part.1.2021.HDCam/Pushpa.The.Rise-Part.1.2021.480p.Hindi.V2.HDCAMRip.x264-DudeFilms.in.mkv`,2],
+['420 IPC','3p3mBhT',`${ms}BollyWood/Zee5/420.IPC.2021/420.IPC.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+['Marakkar Arab Sagar','3IWOY9s',`${ms}South.Movies/Marakkar.Arab.Sagar.Ka.Sher.2021/Marakkar.Arab.Sagar.Ka.Sher.2021.Hindi.Malayalam.480p.WEB-DL.ESub-DudeFilms.in.mkv`,2],
+['Spider-Man No Way Home','30wVbaR',`${ms}HollyWood/Spider-Man.No.Way.Home.2021.CamRip/Spider.Man.No.Way.Home.2021.Hindi.English.480p.CAMRip-DudeFilms.in.mkv`,3],
+['Kurup','30xggSp',`${ms}South.Movies/Kurup.2021/Kurup.2021.480p.WEB-DL.Hindi.DD5.1-Malayalam.ESub.x264-DudeFilms.in.mkv`,2],
+['Chandigarh Kare Aashiqui','3pL0Fr4',`${ms}BollyWood/Chandigarh.Kare.Aashiqui.2021.PreDVD/Chandigarh.Kare.Aashiqui.2021.Hindi.480p.Pre-DVDRip-DudeFilms.in.mkv`,1],
+['Tadap','32SRNYt',`${ms}BollyWood/Tadap.2021.HDcam/Tadap.2021.Hindi.480p.HQ.PreDVD.Rip.x264.AAC-DudeFilms.in.mkv`,1],
+['Bom Biswas','3IfgpLj',`${ms}BollyWood/Zee5/Bob.Biswas.2021/Bob.Biswas.2021.480p.ZEE5.WEB-DL.Esub-DudeFilms.in.mkv`,1],
+['Nothing Is Impossible','3177A5K',`${ms}HollyWood/NetFlix/14.Peaks.Nothing.Is.Impossible.2021/14.Peaks.Nothing.Is.Impossible.2021.480p.WEBRip.Hindi.English.Esubs-DudeFilms.in.mkv`,3],
+['Night of The Sicario','3d0iuMV',`${ms}HollyWood/Night.of.The.Sicario.2021/Night.of.The.Sicario.2021.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
+['Chhorii','3xqWsMF',`${ms}BollyWood/Amazon.Prime/Chhorii.2021/Chhorii.2021.480p.WEB-DL.x264.Hindi.AAC5.1.ESubs%20-DudeFilms.in.mkv`,1],
+['A Castle For Christmas','3CW2CW7',`${ms}HollyWood/NetFlix/A.Castle.For.Christmas.2021/A.Castle.For.Christmas.2021.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
+['Antim','3184Gxb',`${ms}BollyWood/Antim.The.Final.Truth.2021.HDCAMRip/Antim.The.Final.Truth.2021.480p.HDCAMRip.HINDI-DudeFilms.in.mkv`,1],
+['Fast and Furios 9','3nUItLY',`${ms}HollyWood/F9.Hindi.ORG/Fast-9.2021.480p.BluRay.Hindi-English.DD5.1.ESub.x264-DudeFilms.in.mkv`,3],
+['Satyameva Jayate 2','3HP6bRP',`${ms}BollyWood/Satyameva.Jayate.2.2021.HDcam/Satyameva.Jayate.2.2021.Hindi.720p.HQPreDVDRipx264.AAC-DudeFilms.in.mkv`,1],
+['Annaatthe','3FQBCcD',`${ms}South.Movies/Annaatthe.2021/Annaatthe.2021.Hindi-Tamil.DD5.1.480p.WEB-DL.x264-DudeFilms.in.mkv`,2],
+['A Boy ... Christmas','3cLPEiW',`${ms}HollyWood/NetFlix/A.Boy.Called.Christmas.2021/A.Boy.Called.Christmas.2021.480p.WEBRRip.Hindi.English.Esubs-DudeFilms.in.mkv`,3],
+['Bruised','311qeew',`${ms}HollyWood/NetFlix/Bruised.2021/Bruised.2021.480p.WEBRRip.Hindi.English.Esubs-DudeFilms.in.mkv`,3],
+['Dhummas','3HSsKoI',`${ms}Gujarati/Dhummas.2021/Dhummas.2021.Gujarati.480p.HDrip.x264.ESubs-DudeFilms.in.mkv`,1],
+['Bunty Aur Babli 2021','3DCM9al',`${ms}BollyWood/Bunty.Aur.Babli.2021.HDCamRip/Bunty.Aur.Babli.2021.480p.CAMRip.HINDI-DudeFilms.in.mkv`,1],
+['Cash: NOTE-BANDI','3FB7dPa',`${ms}BollyWood/hotstar/Cash.2021/Cash.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+['Dhamaka','3DOhveB',`${ms}BollyWood/NetFlix/Dhamaka.2021/Dhamaka.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+['Home Sweet Home Alone','3CeZfcy',`${ms}HollyWood/Disney+/Home.Sweet.Home.Alone.2021/Home.Sweet.Home.Alone.2021.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
+['Red Notice','30tkFG2',`${ms}HollyWood/NetFlix/Red.Notice.2021/Red.Notice.2021.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv`,3],
+['Shang-Chi','3c26NVy',`${ms}HollyWood/Shang.Chi.2021/Shang.Chi.2021.480p.BluRay.Hindi.Clean-English.ESub.x264-DudeFilms.in.mkv`,3],
+['Gully Rowdy','3kt6bwC',`${ms}South.Movies/Gully.Rowdy.2021/Gully.Rowdy.2021.Hindi-Telugu.480p.HDRip.ESub-DudeFilms.in.mkv`,2],
+['No Time To Die','3qsxYAZ',`${ms}HollyWood/No.Time.To.Die.2021/No.Time.To.Die.2021.Hindi.(CAM).English.480p.HDRip-DudeFilms.in.mkv`,3],
+['Finch','3wDqvjV',`${ms}HollyWood/Finch.2021/Finch.2021.720p.WEBRip.HINDI.DUB.MELBET-DudeFilms.in.mkv`,3],
+["Sooryavanshi","3kacepE",`${ms}BollyWood/Sooryavanshi.2021.HQ.PreDVDRip/Sooryavanshi.2021.Hindi.480p.HQ.Pre.DVDRip-DudeFilms.in.mkv`,1],
+["Marvel Eternals","3ERerhS",`${ms}HollyWood/Eternals.2021.HDcam/Eternals.2021.Hindi.English.480p.CAMRip-DudeFilms.in%20-.mkv`,3],
+["Jai Bhim","3kcnHoK",`${ms}South.Movies/Jai.Bhim.2021/Jai.Bhim.2021.480p.WEB-DL.Hindi-Tamil.DD5.1.ESub.x264-DudeFilms.in.mkv`,2],
+["Hum Do Hamare Do","31m0Alf",`${ms}BollyWood/hotstar/Hum.Do.Hamare.Do.2021/Hum.Do.Hamare.Do.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+["Sardar Udham","https://dl.gdc03.workers.dev/0:/BollyWood/Amazon.Prime/Sardar.Udham.2021/Sardar.Udham.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv",1],
+["Rashmi Rocket","false",`${ms}BollyWood/Zee5/Rashmi.Rocket.2021/Rashmi.Rocket.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+["Sanak (2021)","false",`${ms}BollyWood/hotstar/Sanak.2021/Sanak.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+["Shiddat","false",`${ms}BollyWood/hotstar/Shiddat.2021/Shiddat.2021.Hindi.480p.WEB-DL.ESub-DudeFilms.in.mkv`,1],
+];
+
+var webseries=
+[
+/*change the episod number to '01' */
+/*[name,bitly img id,totalEpisods,fristEpisodeLink],*/
+["The WhistleBlower","3oWvbz0",9,"__wsdomain/WebSeries/Sony.Liv/The.WhistleBlower.S01/The.WhistleBlower.S01.E01.720p.Hindi.WEB-DL.ESub-DudeFilms.in.mkv"],
+["The Witcher","3maR0Jd",8,"__wsdomain/WebSeries/NetFlix/The.Witcher/S01/The.Witcher.S01.E01.Dual.Audio.Hindi.English.720p.HEVC.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Qatil Haseenaon Ke Naam","3rWBWD0",6,"__wsdomain/WebSeries/Zee5/Qatil.Haseenaon.Ke.Naam.S01/Qatil.Haseenaon.Ke.Naam.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Arya Season 2","31KRxKU",8,"__wsdomain/WebSeries/hotstar/Aarya/S02/720p/Aarya.S02.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Money Heist Part 1","3y7cko1",5,"__wsdomain/WebSeries/NetFlix/Money.Heist.S05/480p/Money.Heist.S05.E01.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Hiccups and Hookups","3I485Ov",8,"__wsdomain/WebSeries/Hiccups.and.Hookups.S01/Hiccups.and.Hookups.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Dil Bekaraar","3I70XAJ",10,"__wsdomain/WebSeries/hotstar/Dil.Bekaraar.S01/Dil.Bekaraar.S01.E01.720p.WEB-DL.x264-DudeFilms.in.mkv"],
+["Illegal","3r5pMHr",8,"__wsdomain/WebSeries/Voot/Illegal.S02/Illegal.S02.E01.Hindi.720p.WEB-DL-DudeFilms.in.mkv"],
+["Hawkeye","3DUs4wm",6,"__wsdomain/WebSeries/hotstar/Hawkeye.S01/480p/Hawkeye.S01.E01.480p.x264.Hindi.English.Multi.Esubs-DudeFilms.in.mkv"],
+["The wheel of Time","32bz1ex",6,"__wsdomain/WebSeries/Amazon.Prime/The.Wheel.of.Time.S01/480p/The.Wheel.of.Time.S01.E01.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Your Honor Season 2","30DfgMQ",5,'__wsdomain/WebSeries/Sony.Liv/Your.Honor.S02/Your.Honor.S02.E01.720p.Web.Dl.Esub-DudeFilms.in.mkv'],
+["Lost In Space","3ova992",10,'__wsdomain/WebSeries/NetFlix/Lost.in.Space/S02/480p/Lost.in.Space.S02.E01.480p.Hindi.Eng.WebDL.Esub-DudeFilms.in.mkv'],
+["Akkad Bakkad Rafu Chakkar","3nZX07X",10,'__wsdomain/WebSeries/Amazon.Prime/Akkad.Bakkad.Rafu.Chakkar.S01/Akkad.Bakkad.Rafu.Chakkar.S01.E01.720p.x264-DudeFilms.in.mkv'],
+["Call My Agent","3o75oTf",6,'__wsdomain/WebSeries/NetFlix/Call.My.Agent.Bollywood.S01/Call.My.Agent.Bollywood.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv'],
+["My Name","3BSziAc",8,'__wsdomain/WebSeries/NetFlix/My.Name.S01/My.Name.S01.E01.Hindi.Korean.720p.HEVC.WEB-DL.ESub-DudeFilms.in.mkv'],
+["Tabbar SonyLiv","3FWSBdZ",8,'__wsdomain/WebSeries/Sony.Liv/Tabbar.S01/Tabbar.S01.E01.Hindi.720p.WEB-DL-DudeFilms.in.mkv'],
+["Kota Factory season 2","3ALcKAB",5,'__wsdomain/WebSeries/NetFlix/Kota.Factory/S02/720p/Kota.Factory.S02.E01.Hindi.720p.WEB-DL.Esubs-DudeFilms.in.mkv'],
+["House of Secrets","3v3bYNt",3,'__wsdomain/WebSeries/NetFlix/House.of.Secrets.The.Burari.Deaths.S01/480p/House.of.Secrets.The.Burari.Deaths.S01.E01.480p.WEB-DL.Hindi.ESub.x264-DudeFilms.in.mkv'],
+];
+
+var ads=
+{
+hoodie:[
+`B08SCD1V6G&asins=B08SCD1V6G&linkId=fda65ec5a19319fad13ce965819d1a4b`,
+`B091J1ZK77&asins=B091J1ZK77&linkId=8867cc870acf384bbe534d7d42b82d24`,
+`B08XKGRCC1&asins=B08XKGRCC1&linkId=cbafd18749e897f324a612c6e4c4c3f4`,
+`B08BLQKSJZ&asins=B08BLQKSJZ&linkId=8d2383b7f51e67b9ab8ac3fb1c440a74`,
+`B08JM7TGK8&asins=B08JM7TGK8&linkId=6b79f0a4ab81f61e09aad7beefde543c`,
+],
+Mobile:[
+`B09FKDH6FS&asins=B09FKDH6FS&linkId=ad31c52354666960fb81005ffda84c38`,
+`B089MTJVLD&asins=B089MTJVLD&linkId=c366f10a4285ebf5192dde62b6e928e8`,
+],
+free_fire:[
+`B09HL5R4SG&asins=B09HL5R4SG&linkId=85cdd31243189ef82bb0de1d1e1c8629`,
+`B09LR11YBK&asins=B09LR11YBK&linkId=8584d44c6c4fb40422f8f282bd666f06`,
+`B08DLZYNWY&asins=B08DLZYNWY&linkId=4b2109db4dcf16f16d3b0660e4e546bd`,
+],
+heaters:[
+`B00ABMASXG&asins=B00ABMASXG&linkId=f030d3a2896fabe7bfc131c8c427afef`,
+`B07WMS7TWB&asins=B07WMS7TWB&linkId=7a96fbce9c018b153411ba7e6c5d50a2`,
+`B097R45BH8&asins=B097R45BH8&linkId=b073cf44b2fdf97101cd82f2e960adae`,
+],
+watches:[
+`B08GSRCGRG&asins=B08GSRCGRG&linkId=7603316b9b3ec422a78b7b6068657550`,
+`B08FTFYTN8&asins=B08FTFYTN8&linkId=13e51a897276465cf32d712c22f64659`,
+`B07Q17WT6M&asins=B07Q17WT6M&linkId=2070b54221e50dd2f449b747aa6ff737`,
+],
+shoes:[
+`B074WBRN4X&asins=B074WBRN4X&linkId=8c8900ea4c617ae77f97428ea66d9e40`,
+`B089QK4BTR&asins=B089QK4BTR&linkId=ddbf14c5f6aa965158f01e67ab3cc706`,
+],
+earphones:[
+`B09JCFSD4Z&asins=B09JCFSD4Z&linkId=424f4242d888c2d41cf795bdd2c619d3`,
+`B09BNZLTDZ&asins=B09BNZLTDZ&linkId=4bcaa7276f5413e1894d607e7ec9b77f`,
+`B09K5HBWVL&asins=B09K5HBWVL&linkId=8b9c8a9184e44ac974d71ee5271a724e`,
+`B08XQQ1GM1&asins=B08XQQ1GM1&linkId=b3b4695ad1fb8be3e03879be1a632667`,
+],
+gloves:[
+`B08L6QPD8N&asins=B08L6QPD8N&linkId=3d7db260bd675a49d5ff95fd7ec315b9`,
+],
+}
+/*<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=priyanshu002b-21&language=en_IN&marketplace=amazon&region=IN&placement=${lnk}&show_border=true&link_opens_in_new_window=true"></iframe>*/
+
+var midTemp=movies.length,lastMid=midTemp,
+mvTemp={},
+moviesAry=[];
+
+movies.map(val=>{
+	mvTemp[midTemp]={
+		mid:midTemp,
+		name: val[0],
+		img: "https://bit.ly/"+val[1],
+		src: val[2],
+		catogary: val[3]
+	}
+	moviesAry.push(mvTemp[midTemp]);
+	midTemp--;
+})
+movies=mvTemp;
