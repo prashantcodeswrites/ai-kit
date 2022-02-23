7965dd0cb135e2d7eba4998c09c4491e0420ccda
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-02-23 10:07:59 +0530

    Add files via upload

---
+++ common/js/webSeries.js
@@ -0,0 +1,56 @@
+
+var webseries=
+[
+/*change the episod number to '01' */
+/*[name,bitly img id,totalEpisods,fristEpisodeLink],*/
+
+["Dexter New Blood","3oXSiIY",10,"__wsdomain/WebSeries/Dexter.New.Blood.S01/Dexter.New.Blood.S01.E01.Hindi.English.720p.HEVC.10bit.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Raising Dion","3p0ddLv",9,"__wsdomain/WebSeries/NetFlix/Raising.Dion/S01/Raising.Dion.S01.E01.720p.Hindi.English.Esub-DudeFilms.in.mkv"],
+["Inventing Anna","3I8QBAg",9,"__wsdomain/WebSeries/NetFlix/Inventing.Anna.S01/Inventing.Anna.S01.E01.Hindi.English.720p.HEVC.10bit.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Rocket Boys s1","3onJv2E",8,"__wsdomain/WebSeries/Sony.Liv/Rocket.Boys.S01/Rocket.Boys.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["The Great Indian Murder s1","3AVPAsq",9,"__wsdomain/WebSeries/hotstar/The.Great.Indian.Murder.S01/The.Great.Indian.Murder.S01.E01.720p.HS.WEB-DL.DD+5.1.H.265-DudeFilms.in.mkv"],
+["Feria The Darkest Light","3siBvBb",8,"__wsdomain/WebSeries/NetFlix/Feria.The.Darkest.Light.S01/Feria.The.Darkest.Light.S01.E01.720p.10Bit.Hindi.English.Esubs-DudeFilms.in.mkv"],
+["All of Us are DAEd","35oVTc8",12,"__wsdomain/WebSeries/NetFlix/All.Of.Us.Are.Dead.S01/All.Of.Us.Are.Dead.S01.E01.Hindi-English-Korean.720p.(10bit).WEB-DL.ESub-DudeFilms.in.mkv"],
+["Unpaused Naya Safar","3FT1vbo",5,"__wsdomain/WebSeries/Amazon.Prime/Unpaused.Naya.Safar.S01/Unpaused.Naya.Safar.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Bhaukaal Season 2","3IndHCB",10,"__wsdomain/WebSeries/MxPlayer/Bhaukaal.S02/Bhaukaal.S02.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Human","3nn2PN6",10,"__wsdomain/WebSeries/hotstar/Human.S01/Human.S01.E01.Hindi.720p.WEB-DL.5.1.ESub.x264-DudeFilms.in.mkv"],
+["Parampara Season 1","3mrvfF5",7,"__wsdomain/WebSeries/hotstar/Parampara.S01/Parampara.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["The WhistleBlower","3oWvbz0",9,"__wsdomain/WebSeries/Sony.Liv/The.WhistleBlower.S01/The.WhistleBlower.S01.E01.720p.Hindi.WEB-DL.ESub-DudeFilms.in.mkv"],
+["The Witcher","3maR0Jd",8,"__wsdomain/WebSeries/NetFlix/The.Witcher/S01/The.Witcher.S01.E01.Dual.Audio.Hindi.English.720p.HEVC.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Qatil Haseenaon Ke Naam","3rWBWD0",6,"__wsdomain/WebSeries/Zee5/Qatil.Haseenaon.Ke.Naam.S01/Qatil.Haseenaon.Ke.Naam.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Arya Season 2","31KRxKU",8,"__wsdomain/WebSeries/hotstar/Aarya/S02/720p/Aarya.S02.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Money Heist Part 1","3y7cko1",5,"__wsdomain/WebSeries/NetFlix/Money.Heist.S05/480p/Money.Heist.S05.E01.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Hiccups and Hookups","3I485Ov",8,"__wsdomain/WebSeries/Hiccups.and.Hookups.S01/Hiccups.and.Hookups.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Dil Bekaraar","3I70XAJ",10,"__wsdomain/WebSeries/hotstar/Dil.Bekaraar.S01/Dil.Bekaraar.S01.E01.720p.WEB-DL.x264-DudeFilms.in.mkv"],
+["Illegal","3r5pMHr",8,"__wsdomain/WebSeries/Voot/Illegal.S02/Illegal.S02.E01.Hindi.720p.WEB-DL-DudeFilms.in.mkv"],
+["Hawkeye","3DUs4wm",6,"__wsdomain/WebSeries/hotstar/Hawkeye.S01/480p/Hawkeye.S01.E01.480p.x264.Hindi.English.Multi.Esubs-DudeFilms.in.mkv"],
+["The wheel of Time","32bz1ex",6,"__wsdomain/WebSeries/Amazon.Prime/The.Wheel.of.Time.S01/480p/The.Wheel.of.Time.S01.E01.Hindi.English.480p.WEB-DL.ESub-DudeFilms.in.mkv"],
+["Your Honor Season 2","30DfgMQ",5,'__wsdomain/WebSeries/Sony.Liv/Your.Honor.S02/Your.Honor.S02.E01.720p.Web.Dl.Esub-DudeFilms.in.mkv'],
+["Lost In Space","3ova992",10,'__wsdomain/WebSeries/NetFlix/Lost.in.Space/S02/480p/Lost.in.Space.S02.E01.480p.Hindi.Eng.WebDL.Esub-DudeFilms.in.mkv'],
+["Akkad Bakkad Rafu Chakkar","3nZX07X",10,'__wsdomain/WebSeries/Amazon.Prime/Akkad.Bakkad.Rafu.Chakkar.S01/Akkad.Bakkad.Rafu.Chakkar.S01.E01.720p.x264-DudeFilms.in.mkv'],
+["Call My Agent","3o75oTf",6,'__wsdomain/WebSeries/NetFlix/Call.My.Agent.Bollywood.S01/Call.My.Agent.Bollywood.S01.E01.Hindi.720p.WEB-DL.ESub-DudeFilms.in.mkv'],
+["My Name","3BSziAc",8,'__wsdomain/WebSeries/NetFlix/My.Name.S01/My.Name.S01.E01.Hindi.Korean.720p.HEVC.WEB-DL.ESub-DudeFilms.in.mkv'],
+["Tabbar SonyLiv","3FWSBdZ",8,'__wsdomain/WebSeries/Sony.Liv/Tabbar.S01/Tabbar.S01.E01.Hindi.720p.WEB-DL-DudeFilms.in.mkv'],
+["Kota Factory season 2","3ALcKAB",5,'__wsdomain/WebSeries/NetFlix/Kota.Factory/S02/720p/Kota.Factory.S02.E01.Hindi.720p.WEB-DL.Esubs-DudeFilms.in.mkv'],
+["House of Secrets","3v3bYNt",3,'__wsdomain/WebSeries/NetFlix/House.of.Secrets.The.Burari.Deaths.S01/480p/House.of.Secrets.The.Burari.Deaths.S01.E01.480p.WEB-DL.Hindi.ESub.x264-DudeFilms.in.mkv'],
+];
+
+var numSeries=webseries.length,
+tempSr={},webseriesAry=[];
+for(let i=0; i <webseries.length; i++){
+	var val=webseries[i],
+	links=[];
+	for(let j=1; j<=val[2]; j++){
+		links.push(val[3].replace("__wsdomain/",ms).replace("E01","E"+get2dNum(j)));
+	}
+	tempSr[i]={
+		name: val[0],
+		img: "https://bit.ly/"+val[1],
+		links
+	};
+	webseriesAry.push(tempSr[i]);
+}
+webseries=tempSr;
+function get2dNum(n){
+	return n>9?n:"0"+n;
+}
